8ba5a15433262ea5970a04f1534e540c14c77d75
master <qewrqw@dwfds.com>
2019-04-23 19:35:30 +0300

    404 error

---
+++ app/controllers/ControllerMain.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace controllers;
+
+use core\Controller;
+
+class ControllerMain extends Controller {
+
+    public function action_index() {
+        echo 'Main controller action index'; //TODO delete
+        //TODO action
+    }
+
+    public function action_create(){
+        echo'Main controller action create';
+    }
+    
+}
