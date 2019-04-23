ec87a058d4594c968fae40aedfef9ce32964d39b
master <qewrqw@dwfds.com>
2019-04-23 20:43:41 +0300

    tasks index_action

--- app/controllers/ControllerTasks.php
+++ app/controllers/ControllerTasks.php
@@ -5,18 +5,16 @@ namespace controllers;
 use core\Controller;
 use models\ModelTasks;
 
-class ControllerTasks extends Controller{
-    
+class ControllerTasks extends Controller {
+
     public function __construct() {
         parent::__construct();
-        $this-> model = new ModelTasks();
+        $this->model = new ModelTasks();
     }
-    
-    
+
     public function action_index() {
-        //TODO show all tasks
+        $this->view->tasks = $this->model->all();
+        $this->view->render('tasks_index_view');
     }
-    
-    
-    
+
 }
---
+++ app/views/tasks_index_view.php
@@ -0,0 +1,8 @@
+<h2>All tasks</h2>
+<ul>
+    <?php foreach ($this->tasks as $task ):        ?>
+    <li><?= $task['name'] ?></li>
+    <?php endforeach;?>
+</ul>
+
+
