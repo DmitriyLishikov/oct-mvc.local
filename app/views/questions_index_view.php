f5f70fbab7d7c621fe44e237d6259b6bf13419a8
master <qewrqw@dwfds.com>
2019-04-25 20:00:44 +0300

    example pb api

---
+++ app/views/questions_index_view.php
@@ -0,0 +1,15 @@
+<table id="questions">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Question</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
+<button id="courses" type="button">Get courses</button>
+<button id="questions-btn" type="button">Get questions</button>
+<script src="js/questions.js"></script>
